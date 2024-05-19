4e0edb6f8ecd629bba9b3ab911525fae0097cc65
AGUSTIN <agustinhillar@hotmail.com>
2024-05-19 18:32:38 -0300

    agregando clases

--- entities/bodega.py
+++ entities/bodega.py
@@ -1,17 +1,51 @@
 class Bodega():
     def init(self, nombre, descripcion, historia, fechaUltimaActualizacion, periodoActualizacion):
 
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.fechaUltimaActualizacion = fechaUltimaActualizacion
-        self.periodoActualizacion = periodoActualizacion
-        self.historia = historia
+        self._nombre = nombre
+        self._descripcion = descripcion
+        self._fechaUltimaActualizacion = fechaUltimaActualizacion
+        self._periodoActualizacion = periodoActualizacion
+        self._historia = historia
 
     def toJSON(self):
         return {
-            "nombre" : self.nombre,
-            "descripcion" : self.descripcion,
-            "fechaUltimaActualizacion" : self.fechaUltimaActualizacion,
-            "historia" : self.historia,
-            "periodoActualizacion" : self.periodoActualizacion
+            "nombre" : self.getNombre(),
+            "descripcion" : self.getDescripcion(),
+            "fechaUltimaActualizacion" : self.getFechaUltimaActualizacion(),
+            "historia" : self.getHistoria(),
+            "periodoActualizacion" : self.getPeriodoActualizacion()
         }
+    
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self, valor):
+        if not isinstance(valor, str):
+            raise ValueError("El nombre debe ser una cadena")
+        self._nombre = valor
+    
+    def getDescripcion(self):
+        return self._descripcion
+
+    def setDescripcion(self, valor):
+        if not isinstance(valor, str):
+            raise ValueError("La descripcion debe ser una cadena")
+        self._descripcion = valor
+    
+    def getFechaUltimaActualizacion(self):
+        return self._fechaUltimaActualizacion
+
+    def setFechaUltimaActualizacion(self, valor):
+        self._fechaUltimaActualizacion = valor
+    
+    def getPeriodoActualizacion(self):
+        return self._periodoActualizacion
+
+    def setPeriodoActualizacion(self, valor):
+        self._periodoActualizacion = valor
+    
+    def getHistoria(self):
+        return self._historia
+
+    def setHistoria(self, valor):
+        self._historia = valor
---
+++ entities/tipoUva.py
@@ -0,0 +1,30 @@
+class TipoUva():
+    def init(self, descripcion, nombre):
+
+        self._nombre = nombre
+        self._descripcion = descripcion
+
+    def toJSON(self):
+        return {
+            "nombreTipoUva" : self.getNombre(),
+            "descripcion" : self.getDescripcion()
+        }
+    
+    def esElTipoUva(self, tipo):
+        return self.getNombre() == tipo
+
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self, valor):
+        if not isinstance(valor, str):
+            raise ValueError("El nombre debe ser una cadena")
+        self._nombre = valor
+    
+    def getDescripcion(self):
+        return self._descripcion
+
+    def setDescripcion(self, valor):
+        if not isinstance(valor, str):
+            raise ValueError("La descripcion debe ser una cadena")
+        self._descripcion = valor
